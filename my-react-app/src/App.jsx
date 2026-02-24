2aec2069b330dc2c679475cc884a903357e19bfa
Zizou10 <paolo.pilati93@gmail.com>
2026-02-24 01:23:35 +0100

    Predisposizione Scaffolding + correzione file main.jsx e posizionamento della logica di stato (da App.jsx a Main.jsx) (installazione bootstrap via terminale fallita, forse da riprovare con cd diversa?

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from "react";
-import ArticleList from "./components";
-import ArticleForm from "./components";
-import ArticleItem from ".components";
-import './App.css'
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <Header />
+      <Main />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ my-react-app/src/components/Main.jsx
@@ -0,0 +1,8 @@
+function Main() {
+  return (
+    <h1>
+        DC COMICS
+    </h1>
+  )}
+
+  export default Main;
